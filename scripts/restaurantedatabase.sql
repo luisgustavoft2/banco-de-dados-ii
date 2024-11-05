24548262dfb5221e76217e80d3cff94f5121547e
renatosbz <rrenata12@hotmail.com>
2024-11-05 13:14:10 -0300

    trigger pontos

--- scripts/restaurantedatabase.sql
+++ scripts/restaurantedatabase.sql
@@ -64,3 +64,26 @@ nome VARCHAR(20) NOT NULL,
 senha VARCHAR(20) NOT NULL,
 tipo VARCHAR(20) NOT NULL CHECK (tipo IN ('Administrador', 'Gerente', 'Funcionário'))
 );
+
+--triggers
+DELIMITER //
+CREATE DEFINER = 'root'@'localhost' TRIGGER ponto_por_compra
+AFTER INSERT
+ON venda
+FOR EACH ROW
+BEGIN
+    DECLARE pontos_adicionados INT;
+
+    SET pontos_na_compra = FLOOR(NEW.valor / 10);
+
+    IF pontos_na_compra > 0 THEN
+        UPDATE cliente
+        SET pontos = pontos + pontos_na_compra
+        WHERE id = NEW.id_cliente;
+
+        SELECT CONCAT('Novos pontos adicionados ao cliente: ', pontos_na_compra) AS mensagem;
+    END IF;
+END;
+//
+
+DELIMITER ;
