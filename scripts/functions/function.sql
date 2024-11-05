2a185a715111009913bb44da59e1ac463df01116
Brenorp <breno.2017205649@unicap.br>
2024-11-05 19:08:45 -0300

    remove comments

--- scripts/functions/function.sql
+++ scripts/functions/function.sql
@@ -1,11 +1,11 @@
 
-CREATE FUNCTION calculate_points(purchase_amount DECIMAL(10, 2)) --receives decimal value of the purchase
-RETURNS INT --returns an int
-DETERMINISTIC --for optimization purposes on mysql
+CREATE FUNCTION calculate_points(purchase_amount DECIMAL(10, 2))
+RETURNS INT 
+DETERMINISTIC 
 BEGIN
     DECLARE points INT;
     IF purchase_amount >= 10 THEN
-        SET points = FLOOR(purchase_amount / 10); --rounding up points to full ints 
+        SET points = FLOOR(purchase_amount / 10); 
     ELSE
         SELECT 'Essa compra não gera pontos por estar abaixo de 10 reais' AS message;
     END IF;
