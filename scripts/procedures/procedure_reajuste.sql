32b50819117eea42099db7dcde2429cd0d2859b8
Brenorp <breno.2017205649@unicap.br>
2024-11-05 17:41:23 -0300

    organizando scripts

---
+++ scripts/procedures/procedure_reajuste.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE reajuste(pct_increase DECIMAL(5, 2))
+BEGIN
+    
+    IF pct_increase > 0 THEN
+        UPDATE prato
+        SET valor = valor * (1 + (pct_increase / 100));
+    ELSE
+        SELECT 'Porcentagem negativas não são aceitas. Insira um valor positivo.' AS message;
+    END IF;
+END ;
