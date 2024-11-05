3a71b245996c57e71ac58c337477db63161aee1f
renatosbz <rrenata12@hotmail.com>
2024-11-05 17:37:44 -0300

    trigger_compra_invalida

--- scripts/trigger_compra_invalida.sql
+++ scripts/trigger_compra_invalida.sql
@@ -2,4 +2,14 @@ CREATE DEFINER = 'root'@'localhost' TRIGGER compra_invalida
 BEFORE INSERT ON venda
 FOR EACH ROW
 BEGIN
-    
+    DECLARE disponibilidade_prato BOOLEAN;
+
+    SELECT disponibilidade INTO disponibilidade_prato
+    FROM prato
+    WHERE id = NEW.id_prato;
+
+    IF disponibilidade_prato = FALSE THEN
+        SIGNAL SQLSTATE '45000' --stop the transaction from happening
+        SET MESSAGE_TEXT = 'Seu pedido foi cancelado pois o prato está indisponível';
+    END IF;
+END;
