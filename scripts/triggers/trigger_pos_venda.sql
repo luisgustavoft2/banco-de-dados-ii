5b860d516efda1c25d55f13998af320c06d97c71
Brenorp <breno.2017205649@unicap.br>
2024-11-06 18:35:38 -0300

    modify trigger to account for quantidade = 0

--- scripts/triggers/trigger_pos_venda.sql
+++ scripts/triggers/trigger_pos_venda.sql
@@ -6,4 +6,15 @@ BEGIN
     JOIN usos u ON ingrediente.id = u.id_ingrediente
     SET ingrediente.quantidade = ingrediente.quantidade - 1
     WHERE u.id_prato = NEW.id_prato;
+
+        UPDATE prato
+    SET disponibilidade = FALSE
+    WHERE id = NEW.id_prato
+    AND EXISTS (
+        SELECT 1
+        FROM ingrediente
+        JOIN usos u ON ingrediente.id = u.id_ingrediente
+        WHERE u.id_prato = NEW.id_prato
+        AND ingrediente.quantidade = 0
+    );
 END;
