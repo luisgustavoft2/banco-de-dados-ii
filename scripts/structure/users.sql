05ea8e4e262bfc58362698d375218bf5e7cd1e2f
Gustavo Pereira Oliveira Lima <101191931+custafo@users.noreply.github.com>
2024-11-06 12:28:54 +0000

    procedures permissions

--- scripts/structure/users.sql
+++ scripts/structure/users.sql
@@ -6,4 +6,8 @@ GRANT ALL PRIVILEGES ON restaurante.* TO 'admin'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON restaurante.* TO 'gerente'@'localhost';
 GRANT SELECT, INSERT ON restaurante.* TO 'funcionario'@'localhost';
 
+GRANT EXECUTE ON PROCEDURE restaurante.reajuste TO 'gerente'@'localhost';
+GRANT EXECUTE ON PROCEDURE restaurante.gastar_pontos TO 'funcionario'@'localhost';
+
+
 FLUSH PRIVILEGES;
