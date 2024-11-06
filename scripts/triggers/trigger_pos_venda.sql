5364fcc5facfaaa99ed6d2b660a42ec5a6375573
renatosbz <rrenata12@hotmail.com>
2024-11-06 17:34:21 -0300

    removing alias

--- scripts/triggers/trigger_pos_venda.sql
+++ scripts/triggers/trigger_pos_venda.sql
@@ -2,8 +2,8 @@ CREATE DEFINER = 'root'@'localhost' TRIGGER pos_venda
 AFTER INSERT ON venda
 FOR EACH ROW
 BEGIN
-    UPDATE ingredientes i
-    JOIN usos u ON i.id = u.id_ingrediente
-    SET i.quantidade = i.quantidade - 1
+    UPDATE ingrediente
+    JOIN usos u ON ingrediente.id = u.id_ingrediente
+    SET ingrediente.quantidade = ingrediente.quantidade - 1
     WHERE u.id_prato = NEW.id_prato;
 END;
--- scripts/views/view_ingredient_mais_usado.sql
+++ scripts/views/view_ingredient_mais_usado.sql
@@ -1,19 +1,19 @@
 CREATE VIEW ingrediente_mais_usado_ultimo_mes AS
 SELECT 
-    i.id AS id_ingrediente,
-    i.nome AS nome_ingrediente,
+    ingrediente.id AS id_ingrediente,
+    ingrediente.nome AS nome_ingrediente,
     COUNT(u.id_ingrediente) AS vezes_usado
 FROM 
     venda v
 JOIN 
     usos u ON v.id_prato = u.id_prato
 JOIN 
-    ingrediente i ON u.id_ingrediente = i.id
+    ingrediente ON u.id_ingrediente = ingrediente.id
 WHERE 
     MONTH(v.dia) >= MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
     AND YEAR(v.dia) >= YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
 GROUP BY 
-    i.id, i.nome
+    ingrediente.id, ingrediente.nome
 ORDER BY 
     vezes_usado DESC
 LIMIT 1;
